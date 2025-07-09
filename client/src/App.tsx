59a4fa51301c88bd8cf3f3aa18d6eeb32979834b
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-07-09 19:13:17 -0300

    added react query and initial backend fetching

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
-import { Button } from './components/ui/button';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CreateRoom from './pages/CreateRoom';
+import Room from './pages/Room';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <div>
-      <Button>Hello World</Button>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="room" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
--- client/src/pages/Create-room.tsx
+++
@@ -1,3 +0,0 @@
-export default function CreateRoom() {
-  return <h1>Create Room</h1>;
-}
