4c452face6db37b5c0263a44a45359151cbf4bd1
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-07-09 21:09:48 -0300

    fixed types error, added post route testing

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,7 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 import { env } from './env.ts';
+import { createRoomRoute } from './http/routes/create-room.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -15,11 +16,12 @@ app.register(fastifyCors, {
 });
 
 app.setSerializerCompiler(serializerCompiler);
-app.setSerializerCompiler(validatorCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
 app.get('/health', () => {
   return 'OK';
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
 app.listen({ port: env.PORT });
