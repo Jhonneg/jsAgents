f7dd13c005c0df7c0513988b1018457f0d398780
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-07-11 19:24:35 -0300

    added room tsx

--- client/src/components/CreateRoomForm.tsx
+++ client/src/components/CreateRoomForm.tsx
@@ -41,6 +41,7 @@ export default function CreateRoomForm() {
 
   async function handleCreateRoom({ name, description }: CreateRoomFormData) {
     await createRoom({ name, description });
+    createRoomForm.reset();
   }
 
   return (
