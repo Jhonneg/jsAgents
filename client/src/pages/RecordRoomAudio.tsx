67e1de46c809de92f985251fd37a661b1007ecaa
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-07-12 22:38:45 -0300

    added record room audio compnent

--- client/src/pages/RecordRoomAudio.tsx
+++ client/src/pages/RecordRoomAudio.tsx
@@ -1,3 +1,70 @@
+/** biome-ignore-all lint/suspicious/noConsole: <asdsdasadasd> */
+
+import { useRef, useState } from 'react';
+import { Button } from '@/components/ui/button';
+
+const isRecordingSupported =
+  !!navigator.mediaDevices &&
+  typeof navigator.mediaDevices.getUserMedia === 'function' &&
+  typeof window.MediaRecorder === 'function';
+
 export default function RecordRoomAudio() {
-  return <h1>RecordRoomAudio</h1>;
+  const [isRecording, setIsRecording] = useState(false);
+  const recorder = useRef<MediaRecorder | null>(null);
+
+  function stopRecording() {
+    setIsRecording(false);
+
+    if (recorder.current && recorder.current.state !== 'inactive') {
+      recorder.current.stop();
+    }
+  }
+
+  async function startRecording() {
+    if (!isRecordingSupported) {
+      alert('Seu navegador não suporta gravação');
+      return;
+    }
+
+    setIsRecording(true);
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      },
+    });
+
+    const recorder = new MediaRecorder(audio, {
+      mimeType: 'audio/webm',
+      audioBitsPerSecond: 64_000,
+    });
+
+    recorder.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        console.log(event.data);
+      }
+    };
+
+    recorder.onstart = () => {
+      console.log('Gravação iniciada');
+    };
+
+    recorder.onstop = () => {
+      console.log('Gravação encerrada/pausada');
+    };
+    recorder.start();
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-3">
+      {isRecording ? (
+        <Button onClick={stopRecording}>Parar gravação</Button>
+      ) : (
+        <Button onClick={startRecording}>Gravar Audio</Button>
+      )}
+      {isRecording ? <p>Gravando...</p> : <p>Pausado</p>}
+    </div>
+  );
 }
